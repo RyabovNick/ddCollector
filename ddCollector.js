d1aca86b686bd85d4f2435a70cdecb35a66c3b39
RyabovNick <nikitos.aka.zed@gmail.com>
2019-06-20 18:37:55 +0300

    complete

--- ddCollector.js
+++ ddCollector.js
@@ -1,6 +1,20 @@
 const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 
+// var consoleHolder = console;
+// function debug(bool) {
+//   if (!bool) {
+//     consoleHolder = console;
+//     console = {};
+//     Object.keys(consoleHolder).forEach(function(key) {
+//       console[key] = function() {};
+//     });
+//   } else {
+//     console = consoleHolder;
+//   }
+// }
+// debug(false);
+
 /**
  * Return distance and duration between two coordinates
  * @param {Array} from - Exm: [55.832809, 37.450958]
@@ -9,13 +23,11 @@ const { JSDOM } = jsdom;
 function ddCollector(from, to) {
   const options = {
     resources: 'usable',
-    runScripts: 'dangerously',
+    runScripts: 'dangerously'
   };
 
-  const from_x = from[0];
-  const from_y = from[1];
-  const to_x = to[0];
-  const to_y = to[1];
+  const [from_x, from_y] = from;
+  const [to_x, to_y] = to;
 
   JSDOM.fromFile('index.html', options).then(dom => {
     let document = dom.window.document;
@@ -23,6 +35,8 @@ function ddCollector(from, to) {
     let script = document.createElement('script');
     let t = document.createTextNode(`
     ymaps.ready(getAllWays);
+
+    var routeInfo = {}
   
     async function getAllWays() {
       const [auto, masstransit, pedestrian, bicycle] = await Promise.all([
@@ -32,15 +46,12 @@ function ddCollector(from, to) {
         getRouteInfo('bicycle'),
       ]);
     
-      const routeInfo = {
+      routeInfo = {
         auto,
         masstransit,
         pedestrian,
         bicycle,
       };
-  
-      // save to db here
-      console.log(routeInfo);
     }
     
     function getRouteInfo(routingMode) {
@@ -73,6 +84,26 @@ function ddCollector(from, to) {
 
     script.appendChild(t);
     document.body.appendChild(script);
+
+    // необходимо подождать, пока придёт ответ с яндекса
+    let interval;
+
+    interval = setInterval(yandexResponseChecker, 100);
+
+    function yandexResponseChecker() {
+      // объект routeInfo может быть неопределен
+      if (typeof dom.window.routeInfo !== 'undefined') {
+        // проверяем, что данные пришли (т.е. в переменной что-то есть)
+        if (
+          Object.entries(dom.window.routeInfo).length !== 0 &&
+          typeof dom.window.routeInfo === 'object'
+        ) {
+          // return here
+          console.log(dom.window.routeInfo);
+          clearInterval(interval);
+        }
+      }
+    }
   });
 }
 
--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const ddCollector = require('./ddCollector');
 
-ddCollector([55.832809, 37.450958], [55.827267, 37.437408]);
+ddCollector([55.751249, 37.597423], [55.751872, 37.6024]);
